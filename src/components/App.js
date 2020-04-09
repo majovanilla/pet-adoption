cb2d341784c82b475892af8f28c28cbe7437e277
majovainilla <maria_reyes@me.com>
2020-04-09 12:57:01 -0500

    Add components to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import MainPage from './main/MainPage';
+import Navbar from './Navbar';
 
 const App = () => {
   return (
     <div className="App">
-      < MainPage />
+      <nav className="Navbar">
+        < Navbar />
+      </nav>
+      <main>
+        < MainPage />
+      </main>
     </div>
   );
 };
