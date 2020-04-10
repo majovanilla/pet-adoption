3b780d189c1ce8f34bd53ba8d6392b40e19b01e0
majovainilla <maria_reyes@me.com>
2020-04-10 12:14:13 -0500

    Add filter to searchbar component

--- src/components/main/SearchBar.jsx
+++ src/components/main/SearchBar.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
+import Filter from './Filter';
 
 const SearchBar = () => (
-  <Form.Row className="my-3 justify-content-center">
-    <FormControl type="text" placeholder="My pet is..." className="px-2 mx-2 col-7" />
-    <Button variant="outline-info" className="mx-2 col-4">Find my pet</Button>
-  </Form.Row>
+  <div className="searchBar">
+    <Filter />
+    <Form.Row className="my-3 justify-content-center">
+      <FormControl type="text" placeholder="My pet is..." className="px-2 mx-2 col-7" />
+      <Button variant="outline-info" className="mx-2 col-4">Find my pet</Button>
+    </Form.Row>
+  </div>
 );
 
 export default SearchBar;
