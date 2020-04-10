38c37e066dd05031ddb0c59700fb42595a19f453
majovainilla <maria_reyes@me.com>
2020-04-10 12:11:18 -0500

    Add props to description component

--- src/components/details/Description.jsx
+++ src/components/details/Description.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 
 
-const Description = () => {
-  return (
-    <div></div>
-  );
-};
+const Description = ({
+  id,
+  url,
+  type,
+  species,
+  breeds,
+  colors,
+  age,
+  gender,
+  size,
+  coat,
+  name,
+  description,
+  photos,
+  videos,
+  status,
+  attributes,
+  environment,
+  tags,
+  contact,
+  _links,
+}) => (
+  <div className="description">
+    <h2>
+      {name}
+    </h2>
+  </div>
+);
 
 export default Description;
