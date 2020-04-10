2ead848a1291c77bd0d20dd862d373f4989435db
majovainilla <maria_reyes@me.com>
2020-04-10 12:12:51 -0500

    Add props to pet component

--- src/components/main/Pet.jsx
+++ src/components/main/Pet.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import ImgCarousel from '../details/ImgCarousel';
+import Description from '../details/Description';
+import Links from '../details/Links';
 
 
-const Pet = () => {
-  return (
-    <div></div>
-  );
-};
+const Pet = ({
+  id,
+  url,
+  type,
+  species,
+  age,
+  gender,
+  size,
+  name,
+  photos,
+  status,
+  tags,
+}) => (
+  <div className="">
+    <ImgCarousel />
+    <Description />
+    <Links />
+  </div>
+);
 
 export default Pet;
