78bbd3d25d67d7bff714932b9dde2f3c3e64abad
majovainilla <maria_reyes@me.com>
2020-04-11 15:33:25 -0500

    Add contact and categories components

---
+++ src/components/main/Categories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Categories = () => (
+  <div>
+    <h1>Categories Under Construction</h1>
+  </div>
+);
+
+export default Categories;
---
+++ src/components/main/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Contact = () => (
+  <div>
+    <h1>Contact Under Construction</h1>
+  </div>
+);
+
+export default Contact;
